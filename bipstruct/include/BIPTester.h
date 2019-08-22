99fd1530554e295efba8037d2d96f3c58253e78d
John Coffey <johnco3@gmail.com>
2019-08-22 03:57:45 -0400

    Split into separate source & header files

--- bipstruct/include/BIPTester.h
+++ bipstruct/include/BIPTester.h
@@ -1,3 +1,17 @@
+/*
+** (c) Copyright 2018 Gables Engineering, Inc.  All rights reserved.
+**
+** File name:
+** Author: John P. Coffey
+**
+** DESCRIPTION
+**
+*/
+
+#pragma once
+
+// SYSTEM INCLUDES
+#include <iostream>
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/interprocess/containers/map.hpp>
 #include <boost/interprocess/containers/vector.hpp>
@@ -5,11 +19,19 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/container/scoped_allocator.hpp>
-#include <iostream>
 
-namespace bip = boost::interprocess;
+// APPLICATION INCLUDES
+// DEFINES
+// EXTERNAL FUNCTIONS
+// EXTERNAL VARIABLES
+// CONSTANTS
+// STRUCTS
+// TYPEDEFS
+// FORWARD DECLARATIONS
 
 namespace Shared {
+    namespace bip = boost::interprocess;
+
     // @JC Note changed from Coliru's managed_mapped_file
     // in futile attempt to fix teh read access violation.
     using Segment = bip::managed_shared_memory;
@@ -31,11 +53,11 @@ namespace Shared {
     //! IPC Map alias (uses special allocator).
     template <typename K, typename T> using Map =
         map<K, T, std::less<K>, ScopedAlloc<
-            typename map<K, T>::value_type>>;
+        typename map<K, T>::value_type>>;
 
     //! User struct shared across processes
     struct MyStruct {
-        //! @JC see https://www.boost.org/doc/libs/1_69_0/doc/html/boost/container/constructible__idp55849760.html
+        //! @JC see constructible__idp55849760.html
         //! This nested typedef is required for the boost
         using allocator_type = ScopedAlloc<char>;
 
@@ -72,7 +94,7 @@ namespace Shared {
         //! @JC constructible_with_allocator_suffix<T> constructor.
         //! @JC This is complex stuff!
         template <typename I, typename A = Alloc<char>>
-        MyStruct(std::allocator_arg_t, A alloc, int a, int b, I&& init)
+        MyStruct(std::allocator_arg_t, A alloc, int a, int b, I && init)
             : MyStruct(a, b, Vec<uint8_t>(std::forward<I>(init), alloc))
         {}
 
@@ -82,7 +104,8 @@ namespace Shared {
         //! Nested IPC container of POD data
         Vec<uint8_t> data;
     private:
-        //! Hidden  constructor (@JC not sure why - to hide the allocator as it is not the first arg?).
+        //! Hidden  constructor (@JC not sure why - to hide the
+        //! allocator as it is not the first arg?).
         explicit MyStruct(int a, int b, Vec<uint8_t> data)
             : a{ a }
             , b{ b }
@@ -93,97 +116,16 @@ namespace Shared {
     //! This is the shared IPC alias.
     using Database = Map<String, MyStruct>;
 
-    static inline std::ostream& operator<<(std::ostream& os, Database const& db) {
+    static inline std::ostream& operator<<(
+        std::ostream& os, Database const& db) {
         os << "db has " << db.size() << " elements:";
-
         for (const auto& [k, v] : db) {
             os << " {" << k << ": " << v.a << "," << v.b << ", [";
             for (unsigned i : v.data)
                 os << i << ",";
             os << "]}";
         }
-
         return os;
     }
 }
 
-class A {
-public:
-    A(int)
-    {}
-    bool operator<(const A&) const {
-        return false;
-    }
-};
-
-class B {
-public:
-    B(int, const char*)
-    {}
-};
-
-int
-main()
-{
-    using Shared::MyStruct;
-    Shared::Segment mf(bip::open_or_create, "test.bin", 65536*2);
-    auto mgr = mf.get_segment_manager();
-
-    auto& db = *mf.find_or_construct<Shared::Database>("complex")(mgr);
-
-    // Issues with brace-enclosed initializer list
-    using Bytes = std::initializer_list<uint8_t>;
-
-    // More magic: piecewise construction protocol :)
-    static constexpr std::piecewise_construct_t pw{};
-    using std::forward_as_tuple;
-
-    // @JC Debugging - try constructing with explicit args
-    MyStruct tempStruct(
-        std::allocator_arg,
-        db.get_allocator(), 1, 2,
-        std::initializer_list<uint8_t>{1, 2, 3});
-
-    //! @JC Debugging - Hmm... this does not work
-    //! @JC Do not know why if piecewise_construct
-    //! @JC below works????
-    //MyStruct tempStruct1(
-    //    std::allocator_arg,
-    //    db.get_allocator(),
-    //    forward_as_tuple(1, 2, Bytes{ 1, 2 }));
-    db.emplace(pw, forward_as_tuple("one"), forward_as_tuple(1, 2, Bytes{ 1, 2 }));
-    db.emplace(pw, forward_as_tuple("two"), forward_as_tuple(2, 3, Bytes{ 4 }));
-    db.emplace(pw, forward_as_tuple("three"), forward_as_tuple(3, 4, Bytes{ 5, 8 }));
-
-    std::cout << "\n=== Before updates\n" << db << std::endl;
-
-    // Clumsy:
-    db[Shared::String("one", mgr)] = MyStruct{ std::allocator_arg, mgr, 1, 20, Bytes{ 7, 8, 9 } };
-
-    // As efficient or better, and less clumsy:
-    auto insert_or_update = [&db](auto&& key, auto&& ... initializers) -> MyStruct & {
-        // Be careful not to move twice: https://en.cppreference.com/w/cpp/container/map/emplace
-        // > The element may be constructed even if there already is an element
-        // > with the key in the container, in which case the newly constructed
-        // > element will be destroyed immediately.
-        if (auto insertion = db.emplace(pw, forward_as_tuple(key), std::tie(initializers...)); insertion.second) {
-            return insertion.first->second;
-        } else {
-            return insertion.first->second = MyStruct(
-                std::allocator_arg,
-                db.get_allocator(),
-                std::forward<decltype(initializers)>(initializers)...); // forwarding ok here
-        }
-    };
-
-    insert_or_update("two", 2, 30, Bytes{});
-    insert_or_update("nine", 9, 100, Bytes{ 5, 6 });
-
-    // partial updates:
-    db.at(Shared::String("nine", mgr)).data.push_back(42);
-
-    // For more efficient key lookups in the case of unlikely insertion, use
-    // heterogeneous comparer, see https://stackoverflow.com/a/27330042/85371
-
-    std::cout << "\n=== After updates\n" << db << std::endl;
-}
